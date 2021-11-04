d3719cde262aa35a2e154f013e0bfc30527e353a
Edward Anderson <nilbus@nilbus.com>
2021-11-03 17:40:50 -0700

    Make noise.html available offline

---
+++ noise-offline.js
@@ -0,0 +1,29 @@
+var CACHE_NAME = 'v1';
+var urlsToCache = [
+  'noise.html',
+  'images/n.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'
+];
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
